b4e3ebc4216ebf22073a9bbb3a1ac36b910d0f12
Volodymyr Sauliak <3429638@gmail.com>
2021-06-19 22:28:45 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,12 +2,7 @@
 
 mkdir output
 
-pandoc -s main.md -o output/main.html
-pandoc main.md -o output/main_bare.html
-pandoc -s main.md -o output/main.pdf
-pandoc -s main.md -o output/main.epub
-pandoc -s main.md -o output/main.odt
-pandoc -s main.md -o output/main.docx
+pandoc -s main.md -o output/src
 
 date > output/.build_date.txt
 
