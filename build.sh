bc5453d27bf54b7982487db5d4c0f1e2028b7644
Volodymyr Sauliak <3429638@gmail.com>
2021-06-19 22:38:22 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,12 +2,5 @@
 
 mkdir output
 
-pandoc -s main.md -o output/src
-
-date > output/.build_date.txt
-
-echo "generated_at: $(date)" > variables.yml
-
-mustache variables.yml index.output.html > output/index.html
-mustache variables.yml README.output.md > output/README.md
+cp -r src output
 
